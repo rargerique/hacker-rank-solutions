f052b6d6dad0b819aa02578f1890c6d922efe635
Argerique <renata.argerique@sap.com>
2020-05-16 16:22:26 -0300

    add counting valleys solution

---
+++ Warm-up Challenges/Counting Valleys/Solution.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+        int altitude = 0;
+        int sum = 0;
+        boolean configuresValley = false;
+        for(char movement: s.toCharArray()){
+            if(movement == 'D'){
+                altitude--;
+            } else{
+                altitude++;
+            }
+            if(!configuresValley){
+                configuresValley = altitude <= -1;
+            }
+            if(altitude == 0 && configuresValley){
+                sum++;
+                configuresValley = false;
+            }
+        }
+
+        return sum;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        int result = countingValleys(n, s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
