e44f787f8ac59f01d12f11ccc7843b2f03b79379
Argerique <renata.argerique@sap.com>
2020-05-16 21:22:19 -0300

    add repeated string solution

---
+++ Warm-up Challenges/Repeated String/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        //count on single string
+        StringBuilder builder = new StringBuilder(s);
+        long count = builder.chars().filter(a -> a == 97).count();
+
+        //count on total complete strings
+        long totalStrings = n / (s.length());
+        count = (totalStrings * count);
+
+        int leftOver = (int) (n % (s.length()));
+        String finalSubstring = s.substring(0, leftOver);
+        builder = new StringBuilder(finalSubstring);
+        count += builder.chars().filter(a -> a == 97).count();
+
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
